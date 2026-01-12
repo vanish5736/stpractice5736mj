c7cf0b59ed47bbeb8660afa81f320abd81729d6c
vanish5736 <jriris1127@gmail.com>
2026-01-12 11:15:24 +0000

    파일 제출함 추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,11 @@ with st.expander("❤️  I hid bunnies here"):
     st.write("Here are some bunnies for you!")
     st.image("https://cdn.shopify.com/s/files/1/0040/8997/0777/files/Cute_Bunny_7d_1024x1024.jpg?v=1698453869", caption="it's a bunny", use_container_width=True)
     st.image("https://media.istockphoto.com/id/499124260/photo/white-rabbit-close-up.jpg?s=612x612&w=0&k=20&c=6ubyY4MwngyBjIAKwA8IeBIVYNjZ4nN7StrOlSHSVHo=", caption="another bunny", use_container_width=True)
+
+
+    # 파일 업로드: 파일을 선택하면 BytesIO 객체로 반환됩니다
+uploaded_file = st.file_uploader("Submit your HWP file here.", type=["hwp"])
+if uploaded_file:
+    import pandas as pd
+    df = pd.read_csv(uploaded_file)
+    st.dataframe(df)
